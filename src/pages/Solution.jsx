71cf88c2744d4b4a75cd64510f8df240eac3911f
ZETREY <84082026+ZHKING13@users.noreply.github.com>
2024-06-08 18:14:58 +0000

    add details formation

--- src/pages/Solution.jsx
+++ src/pages/Solution.jsx
@@ -36,16 +36,17 @@ function Solution() {
         productItems.forEach((element) => {
             if (element.id === id) {
                 setProduct(element);
-                console.log(element);
-                        setCurrentProductIndex(productItems.indexOf(element));
+                console.log("prjrriiii",element);
 
             }
         });
     };
     const getElementById = (id) => {
         produits.forEach((element) => {
-            if (element.id === id) {
+            if (element.id == id) {
                 setItem(element);
+                 setCurrentProductIndex(produits.indexOf(element));
+
                 console.log(element);
             }
         });
@@ -62,14 +63,14 @@ function Solution() {
     }, [id]);
     return (
         
-            currentProductIndex && item ? (
+            item ? (
             <div className="container">
             <section className=" mt-20 flex md:flex-row flex-col ">
                 <div className="flex justify-between w-full md:flex-row flex-col items-center gap-10">
                     <div className="md:w-[50%] md:container md:mt-10 pt-8 flex items-start flex-col gap-5 w-full">
                         <div>
                             <h2 className=" title text-primary">
-                                {item?.title}
+                                {product?.title} <span className="text-secondary ">{product?.subtitle} </span>
                             </h2>
                             <img
                                 className="mt-4 object-cover"
@@ -88,7 +89,7 @@ function Solution() {
                     </div>
                 </div>
             </section>
-            <section className=" dark:bg-gray-100 flex flex-col items-center gap-4">
+            <section className=" dark:bg-gray-100 flex flex-col items-center mt-3 gap-4">
                 <div className="  mx-auto flex flex-col items-center my-6 space-y-1 md:text-center">
                     <h2 className="pb-3 heading text-primary uppercase ">
                         L'IMPORTANCE de notre solution
@@ -110,7 +111,7 @@ function Solution() {
                                     className={cn(
                                         "flex w-full sm:w-auto md:min-w-1/4 md:w-1/3 my-2 flex-col items-center md:p-4",
                                         {
-                                            " md:border-l-2 md:border-r-2":
+                                            " md:border-l-2 md:border-r-2 py-1":
                                                 index == 1,
                                         },
                                         {
@@ -134,7 +135,7 @@ function Solution() {
                                         {a?.title}
                                     </h3>
                                     <div className=" leading-tight text-center">
-                                        <p className="text-base text-center">
+                                        <p className="textNormal text-center">
                                             {a?.subtitle}
                                         </p>
                                     </div>
@@ -143,7 +144,7 @@ function Solution() {
                         })}
                 </div>
                 <div className="md:container md:p-4 mx-auto space-y-1 md:text-center">
-                    <h2 className="pb-3  text-lg leading-tight font-bold text-primary uppercase ">
+                    <h2 className="pb-3  heading text-primary  uppercase ">
                         NOTRE APPROCHE PRATIQUE POUR CREER VOTRE ENTREPRISE
                     </h2>
                 </div>
@@ -169,7 +170,7 @@ function Solution() {
                                     {/* <h3 className="my-3 text-lg text-center font-semibold">
                                     {b.title}
                                 </h3> */}
-                                    <div className=" leading-tight text-base text-center">
+                                    <div className=" leading-tight textNormal text-center">
                                         <p>{b.title || b.subtitle || b}</p>
                                     </div>
                                 </div>
@@ -180,7 +181,7 @@ function Solution() {
                     <h2 className="pb-3 heading text-primary uppercase ">
                         Pourquoi nous choisir
                     </h2>
-                    <p className="md:text-center md:w-[75%] text-base ">
+                    <p className="md:text-center md:w-[75%] textNormal ">
                         {replaceTextWithLogo(item?.whyChoose)}
                     </p>
                 </div>
@@ -188,19 +189,21 @@ function Solution() {
                     <h2 className="pb-3 heading text-primary uppercase">
                         Contactez-nous dès maintenant
                     </h2>
-                    <p className="md:text-center md:w-[75%] text-normal text-base ">
+                    <p className="md:text-center md:w-[75%] text-normal textNormal ">
                         {replaceTextWithLogo(item?.contact)}
                     </p>
                 </div>
                 <div className="w-full mt-6 mb-5 flex justify-center gap-8">
-                     <Link to={`/prouits&service/${productItems[currentProductIndex - 1].id}`}>
+                       {
+                    currentProductIndex > 0 &&  <Link to={`/prouits&service/${productItems[currentProductIndex - 1].id}`}>
                                    <Button
                        
                         className={cn("rounded-[45px] px-6 bg-secondary",)}
                     >
-                        precedent
+                        suivant
                     </Button>
-                                </Link>
+                        </Link>
+                  }
                     <Button
                         onClick={handleAddProduct}
                         disabled={products.includes(product)}
@@ -209,14 +212,17 @@ function Solution() {
                     >
                         Ajouter à ma selection
                     </Button>
-                    <Link to={`/prouits&service/${productItems[currentProductIndex + 1].id}`}>
+                    
+                         {
+                    currentProductIndex < productItems.length - 1 &&  <Link to={`/prouits&service/${productItems[currentProductIndex + 1].id}`}>
                                    <Button
                        
                         className={cn("rounded-[45px] px-6 bg-secondary",)}
                     >
                         suivant
                     </Button>
-                                </Link>
+                        </Link>
+                  }
                     
                 </div>
             </section>
