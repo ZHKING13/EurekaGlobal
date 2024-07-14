c6e5e3c264275e1e8b9d95898eab445b8d7ce721
ZETREY <84082026+ZHKING13@users.noreply.github.com>
2024-07-14 09:40:38 +0000

    delete large file

--- src/main.jsx
+++ src/main.jsx
@@ -22,7 +22,8 @@ Aos.init({
    delay: 700, // values from 0 to 3000, with step 50ms
   duration: 400, // values from 0 to 3000, with step 50ms
   easing: 'ease', // default easing for AOS animations
-  once: false, // whether animation should happen only once - while scrolling down
+    once: false, // whether animation should happen only once - while scrolling d
+  
   mirror: false
 });
 const router = createBrowserRouter([
